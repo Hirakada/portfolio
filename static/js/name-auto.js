f7a102f733293bce061ab66767828d8678d99064
Hirakada <hirakada.id@gmail.com>
2025-03-13 20:41:02 +0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   index.html
    	modified:   main.css
    	new file:   static/icon/favicon_io (1)/android-chrome-192x192.png
    	new file:   static/icon/favicon_io (1)/android-chrome-512x512.png
    	new file:   static/icon/favicon_io (1)/apple-touch-icon.png
    	new file:   static/icon/favicon_io (1)/favicon-16x16.png
    	new file:   static/icon/favicon_io (1)/favicon-32x32.png
    	new file:   static/icon/favicon_io (1)/favicon.ico
    	new file:   static/icon/favicon_io (1)/site.webmanifest
    	new file:   static/icon/favicon_io/android-chrome-192x192.png
    	new file:   static/icon/favicon_io/android-chrome-512x512.png
    	new file:   static/icon/favicon_io/apple-touch-icon.png
    	new file:   static/icon/favicon_io/favicon-16x16.png
    	new file:   static/icon/favicon_io/favicon-32x32.png
    	new file:   static/icon/favicon_io/favicon.ico
    	new file:   static/icon/favicon_io/site.webmanifest
    	new file:   static/img/logo-black.svg
    	new file:   static/img/logo-white.svg
    	new file:   static/js/name-auto.js
    
     Changes not staged for commit:
    	deleted:    index.html
    	deleted:    main.css
    	deleted:    static/icon/favicon_io (1)/android-chrome-192x192.png
    	deleted:    static/icon/favicon_io (1)/android-chrome-512x512.png
    	deleted:    static/icon/favicon_io (1)/apple-touch-icon.png
    	deleted:    static/icon/favicon_io (1)/favicon-16x16.png
    	deleted:    static/icon/favicon_io (1)/favicon-32x32.png
    	deleted:    static/icon/favicon_io (1)/favicon.ico
    	deleted:    static/icon/favicon_io (1)/site.webmanifest
    	deleted:    static/icon/favicon_io/android-chrome-192x192.png
    	deleted:    static/icon/favicon_io/android-chrome-512x512.png
    	deleted:    static/icon/favicon_io/apple-touch-icon.png
    	deleted:    static/icon/favicon_io/favicon-16x16.png
    	deleted:    static/icon/favicon_io/favicon-32x32.png
    	deleted:    static/icon/favicon_io/favicon.ico
    	deleted:    static/icon/favicon_io/site.webmanifest
    	deleted:    static/img/logo-black.svg
    	deleted:    static/img/logo-white.svg
    	deleted:    static/js/name-auto.js
    
     Untracked files:
    	.vscode/
    	src/

---
+++ static/js/name-auto.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".auto-type", {
+  strings: ["IRAKADA", "izkya Raka Priananda"],
+  typeSpeed: 80,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+  showCursor: false     
+});
