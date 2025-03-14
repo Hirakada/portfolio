562696cfe89f673355aaef002336c6e73a7779f5
Hirakada <hirakada.id@gmail.com>
2025-03-14 23:24:35 +0700

    new file:   .vscode/launch.json 	modified:   index.html 	modified:   src/assets/css/global.css 	modified:   src/assets/css/main.css 	modified:   src/assets/js/scroll.js

--- src/assets/js/scroll.js
+++ src/assets/js/scroll.js
@@ -3,25 +3,43 @@ document.addEventListener("DOMContentLoaded", function () {
     const backButton = document.getElementById("back");
     const startSection = document.querySelector("header");
     const introductionSection = document.getElementById("introduction");
+    const introductionSectionContent = document.querySelectorAll(".container");
+    console.log(introductionSectionContent);
 
     function scrollToSection(section) {
-        section.scrollIntoView({ behavior: "smooth" });
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
     }
 
-    startButton.addEventListener("click", () => scrollToSection(introductionSection));
-    backButton.addEventListener("click", () => scrollToSection(startSection));
+    if (startButton) {
+        startButton.addEventListener("click", () => scrollToSection(introductionSection));
+    }
 
-    const observer = new IntersectionObserver(([entry]) => {
-        if (entry.isIntersecting) {
-            backButton.classList.add("show");
-            backButton.classList.remove("hide");
-        } else {
-            setTimeout(() => {
-                backButton.classList.add("hide");
-                backButton.classList.remove("show");
-            }, 100); 
-        }
-    }, { threshold: 0.5 });
+    if (backButton) {
+        backButton.addEventListener("click", () => scrollToSection(startSection));
+    }
 
-    observer.observe(introductionSection);
+    if (introductionSection && introductionSectionContent.length > 0 && backButton) {
+        const observer = new IntersectionObserver(([entry]) => {
+            console.log("IntersectionObserver:", entry.isIntersecting); 
+
+            if (entry.isIntersecting) {
+                introductionSectionContent.forEach(container => {
+                    container.classList.add("show");
+                });
+                backButton.classList.add("show");
+                backButton.classList.remove("hide");
+            } else {
+                setTimeout(() => {
+                    backButton.classList.add("hide");
+                    backButton.classList.remove("show");
+                }, 100);
+            }
+        }, { threshold: 0.2 });
+
+        observer.observe(introductionSection);
+    } else {
+        console.warn("One or more elements not found in the DOM.");
+    }
 });
